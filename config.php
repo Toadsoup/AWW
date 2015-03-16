f911b5fe2a74e8d622aa6dcd4a1f7e211395d58f
Toadsoup <toadsoup@gmail.com>
2015-03-15 23:31:09 -0500

    start setting up DB

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 
 echo "Config <br />";
+
+include("libs/Database.php");
+
 include("connect.php");
 include("game.class.php");
 
--- connect.php
+++ connect.php
@@ -2,3 +2,15 @@
 
 
 echo 'Connect<br />';
+
+// DB configuration settings
+$config = array();
+
+    $config['host'] = 'localhost:3306';
+    $config['user'] = 'root';
+    $config['pass'] = '';
+    $config['database'] = 'aww';
+
+// connect to DB
+$db = new DB($config);
+$db->setFetchMode(2);
